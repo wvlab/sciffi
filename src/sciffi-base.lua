8e87ab6dfc21adac0d2e72f4be9ee232857961fb
wvlab <wvlab@protonmail.com>
2025-01-13 22:00:11 +0200

    base: add some type definitions

--- src/sciffi-base.lua
+++ src/sciffi-base.lua
@@ -1,44 +1,55 @@
--- TODO: add types
 -- TODO: check for shell escape
--- TODO: add lua lsp config
 -- TODO: add message and warning API
+
+--- @class Interpretator
+--- @field execute fun(code: string): nil
+
+--- @class (exact) SciFFI
+--- @field interpretators Interpretator[]
+--- @field helpers Helpers
+--- @field private output string
+--- @field public write fun(text: string): nil
+--- @field private flush fun(): nil
 sciffi = {
     interpretators = {},
     output = "",
-
-    write = function(text)
-        sciffi.output = sciffi.output .. text
-    end,
-
-    flush = function()
-        local output, _ = sciffi.output:gsub("\n", " ")
-        tex.print(
-            luatexbase.catcodetables["sciffi@savedtable"],
-            output
-        )
-
-        sciffi.output = ""
-    end,
-
-    helpers = {
-        -- TODO: test somehow????
-        deindent = function(code)
-            code = code:match("^\n*(.-)\n*$")
-
-            local min_indent = math.huge
-            for line in code:gmatch("[^\n]+") do
-                local leading_spaces = line:match("^(%s*)")
-                if #line > 0 then
-                    min_indent = math.min(min_indent, #leading_spaces)
-                end
-            end
-
-            local result = code:gsub("([^\n]+)", function(line)
-                return line:sub(min_indent + 1)
-            end)
-
-            return result
-        end,
-    }
 }
+
+function sciffi.write(text)
+    sciffi.output = sciffi.output .. text
+end
+
+function sciffi.flush()
+    local output, _ = sciffi.output:gsub("\n", " ")
+    tex.print(
+        luatexbase.catcodetables["sciffi@savedtable"],
+        output
+    )
+
+    sciffi.output = ""
+end
+
+--- @class Helpers
+--- @field deindent fun(code: string): string
+local helpers = {}
+function helpers.deindent(code)
+    code = code:match("^\n*(.-)\n*$")
+
+    local min_indent = math.huge
+    for line in code:gmatch("[^\n]+") do
+        local leading_spaces = line:match("^(%s*)")
+        if #line > 0 then
+            min_indent = math.min(min_indent, #leading_spaces)
+        end
+    end
+
+    local result = code:gsub("([^\n]+)", function(line)
+        return line:sub(min_indent + 1)
+    end)
+
+    return result
+end
+
+sciffi.helpers = helpers
+
 return sciffi
