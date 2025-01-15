5926fc2575e1f63d5141ba3f877129514287710e
wvlab <wvlab@protonmail.com>
2025-01-15 02:36:09 +0200

    base: substitute tex code to grab lines with lua implementation

--- src/sciffi-base.lua
+++ src/sciffi-base.lua
@@ -1,12 +1,21 @@
 -- TODO: check for shell escape
 -- TODO: add message and warning API
 
+-- FOR SOME REASON UNKNOWN TO ME THEY FORBID TO CALL `callback.register`
+-- this trick ensures this function will be present
+if luatexbase then
+    local _luatexbase = luatexbase
+    luatexbase.uninstall()
+    luatexbase = _luatexbase
+end
+
 --- @class Interpretator
 --- @field execute fun(code: string): nil
 
 --- @class (exact) SciFFI
---- @field interpretators Interpretator[]
+--- @field interpretators { [string]: Interpretator }
 --- @field helpers Helpers
+--- @field env SciFFIEnv
 --- @field private output string
 --- @field public write fun(text: string): nil
 --- @field private flush fun(): nil
@@ -15,24 +24,84 @@ sciffi = {
     output = "",
 }
 
+--- @param text string
+--- @return nil
 function sciffi.write(text)
     sciffi.output = sciffi.output .. text
 end
 
+--- @return nil
 function sciffi.flush()
     local output, _ = sciffi.output:gsub("\n", " ")
-    tex.print(
-        luatexbase.catcodetables["sciffi@savedtable"],
-        output
-    )
+    tex.print(luatexbase.catcodetables["sciffi@savedtable"], output)
 
     sciffi.output = ""
 end
 
---- @class Helpers
+--- @class SciFFIEnv
+--- @field lines string[]
+--- @field private interpretator string
+--- @field private previous_callback function | nil
+--- @field private callback fun(line: string): string | nil
+--- @field private start fun(interpretator: string): nil
+--- @field private close fun(): nil
+sciffi.env = {
+    lines = {}
+}
+
+--- @param interpretator string
+--- @return nil
+function sciffi.env.start(interpretator)
+    if not sciffi.interpretators[interpretator] then
+        -- TODO: add error printing with future api
+        return
+    end
+    sciffi.env.interpretator = interpretator
+    sciffi.env.previous_callback = callback.find("process_input_buffer")
+    local _, err = callback.register("process_input_buffer", sciffi.env.callback)
+    if err then
+        -- TODO: add error printing with future api
+        texio.write_nl("con", err)
+        sciffi.env.close()
+        return
+    end
+end
+
+--- @param line string
+--- @return string | nil
+function sciffi.env.callback(line)
+    local pos = line:find("\\end{sciffi}")
+    if not pos then
+        table.insert(sciffi.env.lines, line)
+        return ""
+    end
+    local before = line:sub(1, pos - 1)
+    local after = line:sub(pos)
+
+    table.insert(sciffi.env.lines, before)
+    return after
+end
+
+--- @return nil
+function sciffi.env.close()
+    local _, err = callback.register("process_input_buffer", sciffi.env.previous_callback)
+    if err then
+        -- TODO: add error printing with future api
+        return
+    end
+
+    callback.previous_callback = nil
+    sciffi.interpretators[sciffi.env.interpretator].execute(table.concat(sciffi.env.lines, "\n"))
+    sciffi.flush()
+end
+
+--- @class (exact) Helpers
 --- @field deindent fun(code: string): string
-local helpers = {}
-function helpers.deindent(code)
+sciffi.helpers = {}
+
+--- @param code string
+--- @return string
+function sciffi.helpers.deindent(code)
     code = code:match("^\n*(.-)\n*$")
 
     local min_indent = math.huge
@@ -49,7 +118,5 @@ function helpers.deindent(code)
 
     return result
 end
-
-sciffi.helpers = helpers
 
 return sciffi
