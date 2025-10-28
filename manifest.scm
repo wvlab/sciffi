975167a0455a55824956f47beae9d0114e3d14a7
wvlab <me@wvlab.xyz>
2025-10-28 11:22:08 +0200

    chore: add guix manifest file

---
+++ manifest.scm
@@ -0,0 +1,7 @@
+(specifications->manifest
+  (list "texlive-scheme-basic" ; base latex
+        "texlive-luatex"       ; luatex 
+        "texlive-lualibs"      ; utility libraries like lfs
+        "texlive-latexmk"      ; build system for examples
+        "texlive-alegreya"     ; font for docs
+        "texlive-l3build"))    ; build system for the sciffi
